772ac56dfe25c3919148530726e75a3a6e246529
KieranGliver <kieran.glovjunk@gmail.com>
2025-07-29 11:17:10 -0400

    Updated dev config files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    
+    'eqeqeq': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-new-func': 'error',
+    'no-script-url': 'error',
+    
+    'arrow-spacing': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-template': 'error',
+  },
+  ignorePatterns: ['_site/**/*', 'node_modules/**/*'],
+};
