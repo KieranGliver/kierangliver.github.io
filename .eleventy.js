b93f65adf1191ce0f91ef4734e3cafb2dfa443d0
Kieran <kieran.glovjunk@gmail.com>
2025-07-29 11:23:18 -0400

    Dev depend (#11)
    * Changed resume to profile
    
    * Created layout and content for profile page
    
    * Installed prettier and eslint
    
    * Updated dev config files

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,34 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/scripts");
+  
+  eleventyConfig.addWatchTarget("src/styles/");
+  eleventyConfig.addWatchTarget("src/scripts/");
+  
+  eleventyConfig.addFilter("dateFormat", (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  });
+
+  eleventyConfig.setServerOptions({
+    port: 8080,
+    showAllHosts: true,
+  });
+  
+  eleventyConfig.setQuietMode(false);
+  
   return {
     dir: {
       input: "src",
-      output: "_site"
-    }
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
   };
 };
