8054484e5fd118f13fbba344c8e1df04ad72bf2b
Kieran Glover <kieran.glovjunk@gmail.com>
2025-07-29 15:03:34 -0400

    Organized css to use both tailwind and css components

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -51,6 +51,6 @@ document.addEventListener('mousemove', e => {
   const x = (e.clientX / window.innerWidth - 0.5) * 20;
   const y = (e.clientY / window.innerHeight - 0.5) * 20;
 
-  const bg = document.getElementById('background-layer');
+  const bg = document.getElementById('parallax-layer');
   bg.style.transform = `translate(${-x}px, ${-y}px) scale(1.05)`;
 });
